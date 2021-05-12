a326d570205964db185f55749371fc14b0ea668c
sakura <84077286+sakura-temmie@users.noreply.github.com>
2021-05-12 21:00:30 +0900

    Update slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,5 +1,5 @@
-#APIトークン
-API_TOKEN = "xoxb-2068570716225-2055957759730-BiqTIfxreb9Wx596s7O8Xqxt"
+#取得したAPIトークン
+API_TOKEN = "xoxb-xxxxxx-xxxxxxxx-xxxxxxx"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "こんにちは！"
